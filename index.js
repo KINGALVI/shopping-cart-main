6f6a7fddcc8daf2047b6a9ad04de83e7b344aa52
KINGALVI <alvialmahamud@gmail.com>
2022-08-20 09:36:39 +0600

    added javascript,html and css

--- index.js
+++ index.js
@@ -5,3 +5,11 @@ document.getElementById('btn-case-plush').addEventListener('click', function () 
   const NewCaseNumder = PriviousCaseNumber + 1;
   CaseNumberField.value = NewCaseNumder;
 })
+
+document.getElementById('btn-case-defalt').addEventListener('click', function () {
+  const CaseNumberField = document.getElementById('case-btn-field');
+  const CaseNumberFieldString = CaseNumberField.value;
+  const PriviousCaseNumber = parseInt(CaseNumberFieldString);
+  const NewCaseNumder = PriviousCaseNumber - 1;
+  CaseNumberField.value = NewCaseNumder;
+})
