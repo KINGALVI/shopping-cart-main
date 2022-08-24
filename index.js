fcee9bc36997a23f7c482a894caec23e5f87e6a7
KINGALVI <alvialmahamud@gmail.com>
2022-08-24 13:52:46 +0600

    added javascript,html and css

--- index.js
+++ index.js
@@ -20,35 +20,71 @@ function UpdateCaseTotalPrices(NewCaseNumber) {
 document.getElementById('btn-case-plush').addEventListener('click', function () {
   const NewCaseNumber = UpdateCaseNumber(true);
   UpdateCaseTotalPrices(NewCaseNumber);
+  calculatesubtotal()
 })
 document.getElementById('btn-case-defalt').addEventListener('click', function () {
   const NewCaseNumber = UpdateCaseNumber(false);
   UpdateCaseTotalPrices(NewCaseNumber);
+  calculatesubtotal()
 })
-function UpdatePhoneNumber(PhoneNumIncrease){
-  const PhoneNumberField= document.getElementById('phone-number-field');
-  const PhoneNumberString=PhoneNumberField.value;
-  const PriviousPhoneNumber= parseInt(PhoneNumberString);
+
+
+
+function UpdatePhoneNumber(PhoneNumIncrease) {
+  const PhoneNumberField = document.getElementById('phone-number-field');
+  const PhoneNumberString = PhoneNumberField.value;
+  const PriviousPhoneNumber = parseInt(PhoneNumberString);
   let NewphoneNumber;
-  if(PhoneNumIncrease==true){
-    NewphoneNumber = PriviousPhoneNumber+1;
+  if (PhoneNumIncrease == true) {
+    NewphoneNumber = PriviousPhoneNumber + 1;
   }
-  else{
-    NewphoneNumber = PriviousPhoneNumber-1;
+  else {
+    NewphoneNumber = PriviousPhoneNumber - 1;
   }
-  PhoneNumberField.value=NewphoneNumber;
+  PhoneNumberField.value = NewphoneNumber;
   return NewphoneNumber;
 }
+
 function UpdateCaseTotalPrice(NewphoneNumber) {
   const PhoneTotalPrice = NewphoneNumber * 1219;
-  const PhoneTotalElement = document.getElementById('phone-case-total');
+  const PhoneTotalElement = document.getElementById('phone-total');
   PhoneTotalElement.innerText = PhoneTotalPrice;
 }
-document.getElementById('phone-btn-plus').addEventListener('click',function(){
-  const NewphoneNumber=UpdatePhoneNumber(true);
+
+function getElementvalueById(elementId) {
+  const phoneTotalElement = document.getElementById(elementId);
+  const phoneTotalElementSting = phoneTotalElement.innerText
+  const CurrentphoneTotal = parseInt(phoneTotalElementSting);
+  return CurrentphoneTotal;
+}
+
+document.getElementById('phone-btn-plus').addEventListener('click', function () {
+  const NewphoneNumber = UpdatePhoneNumber(true);
   UpdateCaseTotalPrice(NewphoneNumber);
-  });
-document.getElementById('phone-btn-minus').addEventListener('click',function(){
-  const NewphoneNumber=UpdatePhoneNumber(false);
+
+  calculatesubtotal();
+});
+
+document.getElementById('phone-btn-minus').addEventListener('click', function () {
+  const NewphoneNumber = UpdatePhoneNumber(false);
   UpdateCaseTotalPrice(NewphoneNumber);
+  calculatesubtotal();
 })
+
+
+function setTextElementVlueById(elementId, value) {
+  const SubTotalElement = document.getElementById(elementId);
+  SubTotalElement.innerText = value;
+}
+
+function calculatesubtotal() {
+  const CurrentPhoneTotal = getElementvalueById('phone-total');
+  const CurrentCaseTotal = getElementvalueById('Case-Total');
+  const CurrentSudTotal = CurrentCaseTotal + CurrentPhoneTotal;
+  setTextElementVlueById('sub-total', CurrentSudTotal);
+  const texAmountString = (CurrentSudTotal * 0.1).toFixed(2);
+  const texAmount = parseFloat(texAmountString);
+  setTextElementVlueById('tax-total', texAmount);
+  const finalAmount = CurrentSudTotal + texAmount;
+  setTextElementVlueById('final-total', finalAmount);
+}
