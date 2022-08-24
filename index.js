920625d6b6c196a24d19262a9c1cecb8defba680
KINGALVI <alvialmahamud@gmail.com>
2022-08-24 11:01:01 +0600

    added javascript,html and css

--- index.js
+++ index.js
@@ -1,15 +1,54 @@
+function UpdateCaseNumber(IsIncrease) {
+  const CaseNumberField = document.getElementById('case-btn-field');
+  const CaseNumberFieldString = CaseNumberField.value;
+  const PriviousCaseNumber = parseInt(CaseNumberFieldString);
+  let NewCaseNumder;
+  if (IsIncrease == true) {
+    NewCaseNumder = PriviousCaseNumber + 1;
+  }
+  else {
+    NewCaseNumder = PriviousCaseNumber - 1;
+  }
+  CaseNumberField.value = NewCaseNumder;
+  return NewCaseNumder;
+}
+function UpdateCaseTotalPrices(NewCaseNumber) {
+  const CaseTotalPrice = NewCaseNumber * 59;
+  const CaseTotalElement = document.getElementById('Case-Total');
+  CaseTotalElement.innerText = CaseTotalPrice;
+}
 document.getElementById('btn-case-plush').addEventListener('click', function () {
-  const CaseNumberField = document.getElementById('case-btn-field');
-  const CaseNumberFieldString = CaseNumberField.value;
-  const PriviousCaseNumber = parseInt(CaseNumberFieldString);
-  const NewCaseNumder = PriviousCaseNumber + 1;
-  CaseNumberField.value = NewCaseNumder;
+  const NewCaseNumber = UpdateCaseNumber(true);
+  UpdateCaseTotalPrices(NewCaseNumber);
 })
-
 document.getElementById('btn-case-defalt').addEventListener('click', function () {
-  const CaseNumberField = document.getElementById('case-btn-field');
-  const CaseNumberFieldString = CaseNumberField.value;
-  const PriviousCaseNumber = parseInt(CaseNumberFieldString);
-  const NewCaseNumder = PriviousCaseNumber - 1;
-  CaseNumberField.value = NewCaseNumder;
+  const NewCaseNumber = UpdateCaseNumber(false);
+  UpdateCaseTotalPrices(NewCaseNumber);
+})
+function UpdatePhoneNumber(PhoneNumIncrease){
+  const PhoneNumberField= document.getElementById('phone-number-field');
+  const PhoneNumberString=PhoneNumberField.value;
+  const PriviousPhoneNumber= parseInt(PhoneNumberString);
+  let NewphoneNumber;
+  if(PhoneNumIncrease==true){
+    NewphoneNumber = PriviousPhoneNumber+1;
+  }
+  else{
+    NewphoneNumber = PriviousPhoneNumber-1;
+  }
+  PhoneNumberField.value=NewphoneNumber;
+  return NewphoneNumber;
+}
+function UpdateCaseTotalPrice(NewphoneNumber) {
+  const PhoneTotalPrice = NewphoneNumber * 1219;
+  const PhoneTotalElement = document.getElementById('phone-case-total');
+  PhoneTotalElement.innerText = PhoneTotalPrice;
+}
+document.getElementById('phone-btn-plus').addEventListener('click',function(){
+  const NewphoneNumber=UpdatePhoneNumber(true);
+  UpdateCaseTotalPrice(NewphoneNumber);
+  });
+document.getElementById('phone-btn-minus').addEventListener('click',function(){
+  const NewphoneNumber=UpdatePhoneNumber(false);
+  UpdateCaseTotalPrice(NewphoneNumber);
 })
